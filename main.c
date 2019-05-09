167d9fa274f2dc01c87d16a3e9b1a0844db22ce8
SHOROUK <49341067+SHOROUKAWWAD@users.noreply.github.com>
2019-05-09 12:35:27 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,55 @@
+
+
+
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+double a ;
+double I ;
+double M ;
+double A;
+double R ;
+scanf( "%lf", &a) ;
+scanf( "%lf", &I) ;
+ M= a+I;
+  printf(" the sum of C&I= %f\n", M);
+A = a-I;
+ printf(" the difference of the two doubles%f", A);
+R = a*a;
+ printf("the square of C %f\n", R);
+
+
+ int G;
+ int F;
+ int H ;
+ int Z ;
+ scanf("%d",&G);
+ scanf("%d",&F );
+ H=  G + F;
+ Z = G*F ;
+  printf(" the sum of the two integers%d", H);
+  printf("the product of the two integers %d", Z);
+
+  char S;
+  char N;
+
+
+
+   scanf (" %c", &S);
+   getchar();
+   scanf ( "%c",&N );
+    getchar();
+    int B = S+N;
+  int D = S*N;
+   printf (" the product of the two chars %d", D); // the product of the two chars
+   printf ( " the sum of the two chars %d", B);
+
+
+ char X=S+N;
+ printf( "%c", X)
+
+return 0;
 }
