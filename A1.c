f713d4b3feb989bef6df786edf12f68e57553647
SHOROUK <49341067+SHOROUKAWWAD@users.noreply.github.com>
2019-05-09 12:39:17 +0200

    Add files via upload

---
+++ A1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+  int main() {
+
+  double result; //we defined the variable result from type double that allows decimals
+  result= (3.0+1.0)/5.0;
+  printf("the value of 4.0/5.0 is %lf\n ", result);
+  return 0;
+  }
---
+++ assigment.c
@@ -0,0 +1,23 @@
+/*
+JTSK-32011
+A1-P1.C
+SHOROUK GABR AWWAD
+s.awwad@jacobs-university.de
+*/
+
+#include <stdio.h>
+  int main() {
+
+  double result; //we defined the variable result from type double that allows decimals
+  result= (3.0+1.0)/5.0;
+  printf("the value of 4.0/5.0 is %f\n ", result);
+
+
+// we define the result variable from type double that allows decimal but we were dividing two integers that's why we were getting 0.00
+
+
+return 0;
+
+
+  }
+
